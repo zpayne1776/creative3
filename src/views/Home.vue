dd9aa8b3e79514df737ff90ea59899d7c96864f5
Zach Payne <zacharydpayne1@gmail.com>
2020-11-07 15:03:09 -0700

    Added footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
 <div>
   <MemeList :memes="memes" />
+  <div class="footer-container">
+    <div class="footer">
+      <a href="https://github.com/zpayne1776/creative3.git">Visit our Github Page!</a>
+      <p>Josh King and Zach Payne</p>
+      <p><em>Please Laugh</em></p>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -22,3 +29,35 @@ export default {
   },
 }
 </script>
+
+<style>
+  .footer-container{
+    display: flex;
+    background-color: #00e3fc;
+    height: 100px;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    bottom: 0px;
+    margin-bottom: 0px;
+    overflow: hidden;
+    margin-top: auto;
+  }
+
+  .footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 0px;
+  }
+
+  .footer a { font-size: 16px; }
+  .footer p { font-size: 14px; }
+  .footer a,p {
+    text-decoration: none;
+    padding: 0px;
+    margin: 0px;
+    color: white;
+  }
+</style>
